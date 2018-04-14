c767cbdcf7acd04815a1e5ef3c1c49916413bf4e
agakshat <agarwalaks30@gmail.com>
2018-04-13 20:23:58 -0400

    push result files

--- errorbar.py
+++ errorbar.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import json
 
-data = json.loads(open('trained_models/LunarLander-v2_scalars.json').read())
+data = json.loads(open('tr2/newLL20/LunarLander-v2_scalars.json').read())
 means = [x[2] for x in data['test/reward_mean']]
 stds = [x[2] for x in data['test/reward_std']]
 eps = [x[1] for x in data['test/reward_std']]
